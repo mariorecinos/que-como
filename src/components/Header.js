a141f71026abe56683ba8fe7bb38ddf5b3523e06
Mario <recinos.mario68@gmail.com>
2021-11-04 12:23:33 -0700

    setup header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,33 @@
-
+import { Link } from 'react-router-dom';
+import { logOut } from '../services/firebase';
 
 const Header = (props) => {
   return (
     <header>
       <h1>Header</h1>
+      <nav>
+        <ul>
+          {
+            props.user ?
+            <>
+            <li>Welcome, {props.user.displayName}</li>
+            <li>
+              <img
+                  src={props.user.photoURL}
+                  alt={props.user.displayName}
+              />
+            </li>
+            <li onClick={logOut}>Logout</li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            </>
+            :<li>
+              <Link to="/login">Login</Link>
+              </li>
+          }
+        </ul>
+      </nav>
     </header>
   );
 };
