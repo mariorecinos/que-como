d073acc8b1b8eff238c30684137470e9cd802b99
Mario <recinos.mario68@gmail.com>
2021-11-04 08:55:02 -0700

    components on app js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import { useState, useEffect, useRef } from 'react-router-dom';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Show from './pages/Show';
+
+import { auth } from './services/firebase';
+
+import './App.css';
 
 function App() {
   return (
