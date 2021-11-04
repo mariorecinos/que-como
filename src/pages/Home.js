fd993ff3f5e788a442fc7d0fa66bc93f1e37a872
Mario <recinos.mario68@gmail.com>
2021-11-04 08:37:05 -0700

    Added Pages

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+
+
+const Home = (props) => {
+  return (
+    <main>
+      <h1>Home</h1>
+    </main>
+  );
+};
+
+export default Home;
---
+++ src/pages/Show.js
@@ -0,0 +1,11 @@
+
+
+const Show = (props) => {
+  return (
+    <main>
+      <h1>Show</h1>
+    </main>
+  );
+};
+
+export default Show;
