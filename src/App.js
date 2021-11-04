e67fb1dc9ed97813cf5b120b55b8e6d5c58f4547
Mario <recinos.mario68@gmail.com>
2021-11-04 08:52:11 -0700

    Router Path

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import { useState, useEffect, useRef } from 'react-router-dom';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import './App.css';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <>
+    <Header />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/Dashboard">
+        <Dashboard />
+      </Route>
+    </Switch>
+    <Footer />
     </>
   );
 }
